809fe0cbac27742ab4b77662914e579d8fe19991
HoboRiceone <54645631+HoboRiceone@users.noreply.github.com>
2020-12-26 14:09:39 -0500

    sub-1.0

--- QPO_preprocessing.py
+++ QPO_preprocessing.py
@@ -2,29 +2,116 @@ import numpy as np
 import pandas as pd
 import gc
 import time
+import re
+import random
+from scipy import stats
 
-def processing(filename):
-    raw_df = pd.read_csv(filename)
-    raw_df = raw_df[raw_df.lang=='en']
-    df = raw_df[['id','created_at','user','text']]
-    
-    del raw_df
-    gc.collect()
-    
-    df.insert(0, 'target', 0)
-    df.insert(3, 'flag', 'NO_QUERY')
-    
-    df.columns=['target','ids','date','flag','user','text']
-    
-    df=df.reset_index(drop=True)
-    gc.collect()
-    
-    for index in range(df.index.size):
-        user = df.loc[index,'user']
-        userid = int(user.split(',')[0].split(':')[1])
-        df.loc[index,'user'] = userid
-        userdate = df.loc[index,'date'].split()
-        df.loc[index,'date'] = userdate[0]+' '+userdate[1]+' '+userdate[2]+' '+userdate[3]
-        day = userdate[1]+userdate[2]
+class pp_model:
+    def __init__(self, filename):
+        raw_df = pd.read_csv(filename, encoding='utf-8') #read data file to pandas
         
-    return df
+        raw_df = raw_df[raw_df.lang=='en'] #remove english tweet
+        self.df = raw_df[['id','created_at','user','text']]
+        self.df.insert(0, 'target', 2)
+        self.df.insert(3, 'flag', 'NO_QUERY')
+        self.df.columns=['target','ids','date','flag','user','text'] #all features
+        
+        del raw_df
+        gc.collect()
+        
+    def processing(self):
+        t_df=self.df.reset_index(drop=True)
+
+        for index in range(t_df.index.size):
+            user = t_df.loc[index,'user']
+            userid = int(user.split(',')[0].split(':')[1]) #extract user id 
+            t_df.loc[index,'user'] = userid
+            userdate = t_df.loc[index,'date'].split()
+            t_df.loc[index,'date'] = userdate[0]+' '+userdate[1]+' '+userdate[2]+' '+userdate[3]
+            day = userdate[1]+userdate[2] #keep useful time information
+            usertext = t_df.loc[index,'text']
+            usertext = re.sub('[^0-9A-Za-z]', ' ', usertext) #remove punctuation
+            t_df.loc[index,'text'] = usertext
+
+        return t_df
+    
+    def userBasedSample(self, t_df, filterWay = 1):
+        # filterWay: 1 use mean as filter number
+        #            2 use mode
+        #            3 use median
+    
+        userdict=dict()
+        for index in t_df.index:
+            user = t_df.loc[index,'user']
+            user = str(user)
+            if (userdict.__contains__(user)): #group by
+                userdict[user].append(index)
+            else:
+                userdict[user] = [index]
+
+        numByUser = [len(listByUser) for listByUser in userdict.values()] #count the number
+        
+        if(filterWay == 1):
+            sampleNum = int(round(np.mean(numByUser)))
+        elif(filterWay == 2):
+            sampleNum = int(stats.mode(numByUser)[0][0])
+        elif(filterWay == 3):
+            sampleNum = int(np.median(numByUser))
+
+        reList = []
+        for listByUser in userdict.values():
+            if(len(listByUser) > sampleNum):
+                reList.extend(random.sample(listByUser, sampleNum)) # take the sampling
+            else:
+                reList.extend(listByUser)
+
+        reList=sorted(reList)
+
+        t_df=t_df.loc[reList]
+        t_df=t_df.reset_index(drop=True)
+        gc.collect()
+        
+        return t_df
+
+    def dateBasedSample(self, t_df):
+        datedict = dict()
+        for index in t_df.index:
+            time = t_df.loc[index,'date']
+            date = time.split()[1]+time.split()[2]
+            if (datedict.__contains__(date)):#group by
+                datedict[date].append(index)
+            else:
+                datedict[date] = [index]
+
+        numByDate = [len(listByDate) for listByDate in datedict.values()]#count the number
+        sampleNum = min(numByDate)
+        reList = []
+        for listByDate in datedict.values():
+            reList.extend(random.sample(listByDate, sampleNum))# take the sampling
+
+        reList=sorted(reList)
+
+        t_df=t_df.loc[reList]
+        t_df=t_df.reset_index(drop=True)
+        gc.collect()
+        
+        return t_df
+
+    def full_processing(self):
+        t_df=self.df.reset_index(drop=True)
+
+        for index in range(t_df.index.size):
+            user = t_df.loc[index,'user']
+            userid = int(user.split(',')[0].split(':')[1])
+            t_df.loc[index,'user'] = userid
+            userdate = t_df.loc[index,'date'].split()
+            t_df.loc[index,'date'] = userdate[0]+' '+userdate[1]+' '+userdate[2]+' '+userdate[3]
+            day = userdate[1]+userdate[2]
+            usertext = t_df.loc[index,'text']
+            usertext = re.sub('[^0-9A-Za-z]', ' ', usertext)
+            t_df.loc[index,'text'] = usertext
+        
+        t_df = self.userBasedSample(t_df, filterWay = 1)
+        t_df = self.dateBasedSample(t_df)
+    
+        return t_df
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # Quantifying-public-opinion-on-certain-company
-This is a social-network-mining project to quantify public opinion on certain company 
+This is a social-network-mining project to quantify public opinion on certain company.
+The whole idea of this project is based on the class CIS600 of Syracuse University.
+
+## Requirements
+### For this you will need:
+* twitter
+* time
+* pandas 
+* wordcloud
+* nltk
+* matplotlib
+* re
+
+These can be installed via:
+pip install twitter
+pip install times
+pip install pandas
+pip install wordcloud
+pip install nltk
+pip install matplotlib
+pip install re
+
+### Setup:
+You must first set up a Twitter APP
+Input this data into the config file with your keys.
+
+For using the preprocessing module, please copy the QPO_preprocessing.py file to the path where you run the training program and 
+import QPO_preprocessing
+
+To apply the preprocessing, you need to create a preprocessing model first. For example:
+model = pp_model(¡®Dell.csv¡¯)
+The only parameter is the file path which you want to train.
+
+After creation, you can apply the basic process ¡®model.processing()¡¯, the sampling based on user ¡®model.userBasedSample(df, 1)¡¯ and the sampling based on date ¡®model.dateBasedSample(df)¡¯ Or, you can just use ¡®model.full_processing()¡¯ to get all the preprocessing effects.
+
+
+For emotion recognition:
+h5py==2.9.0
+Keras==1.1.0
+numpy==1.16.0
+pandas==0.24.1
+python-dateutil==2.8.0
+pytz==2018.9
+PyYAML==5.1
+scipy==1.2.1
+six==1.12.0
+Theano==1.0.4
+
+For another method please download the newest textblob. 
+Use any python IDE and modify the path for tweets input in file SA.py and demo.py
+
